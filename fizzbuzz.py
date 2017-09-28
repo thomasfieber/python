cccad46d0ec62cac17589f92bb89c1d20e1bfd9d
Thomas Fieber <thomfieber@gmail.com>
2017-09-28 05:50:41 -0400

    Fixed spacing issues

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,19 +10,22 @@ Assumptions:
     - We will print out numbers starting at 1, and will include the limit.
 
 '''
+
+
 def fizzbuzz(n):
     '''
     This function takes in one argument, n. We will print numbers 1 through n,
     replacing "fizz", "buzz", and "fizzbuzz" where appropriate.
     '''
     for i in range(n + 1)[1:]:
-        if i % 15 == 0: # if n is divisible by 3 and 5 print 'fizzbuzz'
+        if i % 15 == 0:  # if n is divisible by 3 and 5 print 'fizzbuzz'
             print("fizzbuzz")
-        elif i % 5 == 0: # check if divisible by 5
+        elif i % 5 == 0:  # check if divisible by 5
             print("buzz")
-        elif i % 3 == 0: # check if divisible by 3
+        elif i % 3 == 0:  # check if divisible by 3
             print("fizz")
         else:
-            print(i) # Otherwise, print the number
+            print(i)  # Otherwise, print the number
+
 
 fizzbuzz(100)
